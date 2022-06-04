95e441d5de486f5d9e312ad7cb36fc1c72680311
Guitenbay <gtenbay@163.com>
2022-06-04 18:45:57 +0800

    完善 README.md

---
+++ READMD.md
@@ -0,0 +1,17 @@
+# 获取 Github Issue 数据脚本
+
+## 安装
+
+    $ npm install
+
+## 配置
+
+在 `config.json` 里配置 `github-access-token`
+
+## 运行
+
+    $ npm start
+
+## 结果
+
+结果在 out 目录下
--- src/main.js
+++ src/main.js
@@ -2,10 +2,6 @@ const { Octokit } = require("@octokit/core");
 const fs = require("fs");
 const config = require("../config.json");
 
-// const args = process.argv.slice(2);
-
-// const [owner, repo] = args;
-
 // Octokit.js
 // https://github.com/octokit/core.js#readme
 const octokit = new Octokit({
@@ -13,7 +9,6 @@ const octokit = new Octokit({
 });
 
 async function getIssueList(owner, repo, page) {
-  console.log(`GET issue/list page=${page}...`);
   const issues = await octokit.request(
     "GET /repos/{owner}/{repo}/issues?state=closed&per_page=100&page={page}",
     {
@@ -30,6 +25,9 @@ async function getIssueList(owner, repo, page) {
     Array.isArray(issues.data) &&
     issues.data.length > 0
   ) {
+    console.log(
+      `GET /${owner}/${repo}/issues page=${page}\t...\t${issues.data.length}`
+    );
     const curr = issues.data.map(
       ({ number, created_at, updated_at, closed_at }) => ({
         issue: `#${number}`,
@@ -38,48 +36,61 @@ async function getIssueList(owner, repo, page) {
         closed_at,
       })
     );
-    return curr.concat(getIssueList(owner, repo, page + 1));
+    const next = await getIssueList(owner, repo, page + 1);
+    return curr.concat(next);
   }
+  console.log(`GET /${owner}/${repo}/issues page=${page}\t...\t${0}`);
   return [];
 }
 
-const repoList = [
-  "https://github.com/apache/zookeeper",
-  "https://github.com/apache/rocketmq",
-  "https://github.com/apache/freemarker",
-  "https://github.com/apache/dubbo",
-  "https://github.com/apache/pdfbox",
-  "https://github.com/apache/flume",
-  "https://github.com/apache/maven",
-  "https://github.com/apache/skywalking",
-  "https://github.com/apache/druid",
-  "https://github.com/apache/hudi",
-  "https://github.com/alibaba/fastjson",
-  "https://github.com/apache/opennlp",
-  "https://github.com/jhy/jsoup",
-  "https://github.com/jmrozanec/cron-utils",
-  "https://github.com/uniVocity/univocity-parsers",
-  "https://github.com/apache/nifi",
-  "https://github.com/redis/jedis",
-  "https://github.com/alibaba/arthas",
-  "https://github.com/alibaba/canal",
-  "https://github.com/alibaba/jvm-sandbox",
-  "https://github.com/alibaba/jetcache",
-  "https://github.com/alibaba/Alink",
-  "https://github.com/alibaba/Sentinel",
-].map((url) =>
-  url.replace(
-    /^https:\/\/github.com\/([^\/]+)\/([^\/]+)$/,
-    (_, p1, p2) => `${p1},${p2}`
-  ).split(',')
-);
+let repoList = [];
 
-repoList.forEach(([owner, repo]) => {
-  getIssueList(owner, repo, 1).then((issues) => {
+const args = process.argv.slice(2);
+
+if (args.length < 2) {
+  repoList = [
+    "https://github.com/apache/zookeeper",
+    "https://github.com/apache/rocketmq",
+    "https://github.com/apache/freemarker",
+    "https://github.com/apache/dubbo",
+    "https://github.com/apache/pdfbox",
+    "https://github.com/apache/flume",
+    "https://github.com/apache/maven",
+    "https://github.com/apache/skywalking",
+    "https://github.com/apache/druid",
+    "https://github.com/apache/hudi",
+    "https://github.com/alibaba/fastjson",
+    "https://github.com/apache/opennlp",
+    "https://github.com/jhy/jsoup",
+    "https://github.com/jmrozanec/cron-utils",
+    "https://github.com/uniVocity/univocity-parsers",
+    "https://github.com/apache/nifi",
+    "https://github.com/redis/jedis",
+    "https://github.com/alibaba/arthas",
+    "https://github.com/alibaba/canal",
+    "https://github.com/alibaba/jvm-sandbox",
+    "https://github.com/alibaba/jetcache",
+    // "https://github.com/alibaba/Alink",
+    "https://github.com/alibaba/Sentinel",
+  ].map((url) =>
+    url
+      .replace(
+        /^https:\/\/github.com\/([^\/]+)\/([^\/]+)$/,
+        (_, p1, p2) => `${p1},${p2}`
+      )
+      .split(",")
+  );
+} else {
+  repoList = [args];
+}
+
+async function main() {
+  for ([owner, repo] of repoList) {
+    const issues = await getIssueList(owner, repo, 1);
     fs.writeFileSync(
       `./out/issue_${owner}_${repo}.json`,
       JSON.stringify(issues, null, 2)
     );
-  });
-})
-
+  }
+}
+main();

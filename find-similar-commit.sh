fd7ca355b0564c98689087a24c675588120682f5
Hummel Mao <gtenbay@163.com>
2022-12-09 20:16:28 +0800

    feat: 脚本添加日志

--- find-similar-commit.sh
+++ find-similar-commit.sh
@@ -3,11 +3,15 @@
 usage="\nError: use args, such as start, stop and restart \n"
 
 start=0
-end=50
+end=5
 step=5
 
 block=10
 
 for ((i = start; i < end; i += step)); do
+  time=$(date "+%Y-%m-%d %H:%M:%S")
+  index=$(expr $i + 1)
+  echo "> [${time}] (${index}) START getting similar commit range (${i}, ${block})..."
   touch out-c/file_$i.txt && npm run commit $i $block > out-c/file_$i.txt 2>&1
 done
+echo '> DONE! <'

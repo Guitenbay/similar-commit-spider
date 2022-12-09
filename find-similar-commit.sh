03a063945c778150d3e1e014e5b877d03beb90d5
Hummel Mao <gtenbay@163.com>
2022-12-09 20:04:34 +0800

    fix: 优化脚本自动化

--- find-similar-commit.sh
+++ find-similar-commit.sh
@@ -2,9 +2,12 @@
 
 usage="\nError: use args, such as start, stop and restart \n"
 
-limit=90
+start=0
+end=50
+step=5
+
 block=10
 
-for ((i = 0; i <= limit; i += 5)); do
+for ((i = start; i < end; i += step)); do
   touch out-c/file_$i.txt && npm run commit $i $block > out-c/file_$i.txt 2>&1
 done
